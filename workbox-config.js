ef334d924670399f2d9fc4c669251c82bc6cd2dc
Ian Cabigon <iancabigon@gmail.com>
2018-08-23 13:47:23 +0800

    added manifest and changed the colors to teal

---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": ".",
+  "globPatterns": [
+    "**/*.{yml,markdown,html,js,jpg,png,svg,css,ico,eot,ttf,woff,woff2,gif,xml,md,lock}"
+  ],
+  "swDest": "sw.js"
+};
